805f75a41968c4dd5b8d71e4558a097f3cbcdcfc
Jon Pither <jon.pither@gmail.com>
2013-09-26 10:14:39 +0100

    this tests passes dawg

--- test/clj_components/test/zk.clj
+++ test/clj_components/test/zk.clj
@@ -1,18 +1,57 @@
 (ns clj-components.test.zk
   (:use [clojure.test]
-        [clj-components.component])
+        [clj-components.component]
+        [avout.core])
   (:require [clj-components.bootstrap :as bootstrap]
-            [clj-components.system :as system]))
+            [clj-components.system :as system]
+            [clj-components.settings :as settings]))
 
 ;; test the bootstrapping
+
+(def ^:dynamic some-val :a-val)
+
+(defn fixture [f]
+  (binding [clj-components.config/zk-root (constantly :clj-components-test)]
+    (f)
+    (bootstrap/shutdown!)))
+
+(use-fixtures :each fixture)
 
 (defrecord TestComponent []
   SystemComponent
   (registry-key [this] :test)
   (init [this settings]
-    (assoc this :booted-with-settings settings)))
+    (assoc this
+      :booted-with-settings settings
+      :some-val some-val)))
+
+(defrecord TestBounceableComponent []
+  SystemComponent
+  BounceOnConfigChange
+  (registry-key [this] :test-bouncy)
+  (init [this settings]
+    (assoc this :some-val some-val)))
 
 (deftest can-boot-up-with-bootstrap-args
-  (binding [clj-components.config/zk-root (constantly :clj-components-test)]
-    (bootstrap/init! {:bootstrap-foo :bootstrap-bar} [clj-components.test.zk/->TestComponent])
-    (is (= {:bootstrap-foo :bootstrap-bar} (-> system/components :test :booted-with-settings) ))))
+  (bootstrap/init! {:bootstrap-foo :bootstrap-bar} [clj-components.test.zk/->TestComponent])
+  (is (= :bootstrap-bar (-> system/components :test :booted-with-settings :bootstrap-foo))))
+
+(deftest can-bounce-components-based-off-zk-config-change
+  (alter-var-root #'some-val (constantly :first-pass))
+
+  (bootstrap/init! {} [clj-components.test.zk/->TestComponent
+                       clj-components.test.zk/->TestBounceableComponent])
+
+  (is (= :first-pass (-> system/components :test :some-val)))
+  (is (= :first-pass (-> system/components :test-bouncy :some-val)))
+
+  (alter-var-root #'some-val (constantly :second-pass))
+
+  (let [client (:client settings/config)]
+    (dosync!! client
+              (alter!! settings/settings assoc :poke-settings :poked)))
+
+  (Thread/sleep 1000)
+
+  (is (= :first-pass (-> system/components :test :some-val)))
+  (is (= :second-pass (-> system/components :test-bouncy :some-val))))
