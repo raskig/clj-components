7029b03b458f1cfeaf2f00380a96577fc91eefd8
Jon Pither <jon.pither@gmail.com>
2013-09-26 10:48:35 +0100

    es tweaks

--- src/clj_components/components/elasticsearch.clj
+++ src/clj_components/components/elasticsearch.clj
@@ -12,13 +12,12 @@
     (let [es-url (:es-url settings)
           es-cluster (:es-cluster settings)]
       (if (and es-url es-cluster)
-        (let [_ (log/info (format "Connecting to %s on cluster %s" es-url es-cluster))
-              pairs (->> (clojure.string/split (:es-url settings) #",")
+        (let [pairs (->> (clojure.string/split (:es-url settings) #",")
                          (map #(clojure.string/split % #":"))
                          (map #(vector(first %) (Integer/parseInt (last %)))))]
-
+          (log/info (format "Connecting to %s on cluster %s" es-url es-cluster))
           (assoc this
-            :es-url es-url
-            :es-cluster es-cluster
-            :es-client (native/connect! pairs {"cluster.name" (:es-cluster settings)})))
+            :url es-url
+            :cluster es-cluster
+            :client (native/connect! pairs {"cluster.name" (:es-cluster settings)})))
         this))))
