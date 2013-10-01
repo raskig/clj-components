2c38087fb599a565d4ad564bb422a22ab69711fa
Jon Pither <jon.pither@gmail.com>
2013-10-01 08:32:46 +0100

    beefing session timeout

--- src/clj_components/config.clj
+++ src/clj_components/config.clj
@@ -15,7 +15,7 @@
 (defrecord ConfigComponent [client settings])
 
 (defn fetch! [zk-connection-watcher ref-watcher]
-  (let [client (connect (zk-ips) :watcher zk-connection-watcher)
+  (let [client (connect (zk-ips) :timeout-msec 10000 :watcher zk-connection-watcher)
         settings (zk-ref client (str "/" (name (zk-root))))]
     (add-watch settings nil (fn [& args] (ref-watcher)))
     (ConfigComponent. client settings)))
