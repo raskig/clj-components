71171b96ffd7495bcf2bee9191328ab0fc6e202b
Benedek Fazekas <benedek.fazekas@mailonline.co.uk>
2013-10-04 12:21:21 +0100

    bumps up the version to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-components "0.1.3-beta4"
+(defproject clj-components "0.1.3-beta5-SNAPSHOT"
   :description "Component lifecycle management lib based off Avout/Zookeeper"
   :url "https://github.com/MailOnline/clj-components"
   :license {:name "Eclipse Public License"
