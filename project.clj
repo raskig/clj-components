ce9c6513d4ab0d2ffd832b9f75683e90fb191daa
Jon Pither <jon.pither@gmail.com>
2013-10-21 17:09:56 +0100

    webserver has smarts for access logs

--- project.clj
+++ project.clj
@@ -14,6 +14,8 @@
                  ;; Logging should be managed by host application no?
                  [org.clojure/tools.logging "0.2.6"]
                  [ch.qos.logback/logback-classic "1.0.7"]
+                 [ch.qos.logback/logback-access "1.0.13"]
+                 [ch.qos.logback/logback-core "1.0.13"]
                  [net.logstash.logback/logstash-logback-encoder "1.2"]
 
                  ;; ElasticSearch Component
--- src/clj_components/components/web_server.clj
+++ src/clj_components/components/web_server.clj
@@ -1,23 +1,46 @@
 (ns clj-components.components.web-server
   (:use [clj-components.component])
   (:require [clojure.tools.logging :as log]
-            [ring.adapter.jetty :as jetty]))
+            [ring.adapter.jetty :as jetty]
+            [clojure.java.io])
+  (:import [ch.qos.logback.access.jetty RequestLogImpl]
+           [org.eclipse.jetty.server NCSARequestLog]
+           [org.eclipse.jetty.util.thread QueuedThreadPool]
+           [org.eclipse.jetty.server Server Request]
+           (org.eclipse.jetty.server.handler HandlerCollection ContextHandlerCollection
+                                             RequestLogHandler)))
 
-(defn- graceful-restart [jetty]
-    (.setGracefulShutdown jetty 10000)
-    (.setStopAtShutdown jetty true))
+(defn ^Server run-jetty-hacked [options & handlers]
+  (let [handler-collection (HandlerCollection.)
+        ^Server s (#'jetty/create-server (dissoc options :configurator))
+        ^QueuedThreadPool p (QueuedThreadPool. ^Integer (options :max-threads 50))]
+    (doseq [h handlers]
+      (.addHandler handler-collection (if (fn? h) (#'jetty/proxy-handler h) h)))
+    (doto s
+      (.setHandler handler-collection)
+      (.setThreadPool p)
+      (.setGracefulShutdown 10000)
+      (.setStopAtShutdown true)
+      (.start))))
+
+(defn- request-log-handler []
+  (doto (RequestLogHandler.)
+    (.setRequestLog
+     (doto (RequestLogImpl.) (.setResource "/logback-access.xml")))))
 
 (defrecord WebServerComponent []
   SystemComponent
   (registry-key [this] :web-server)
 
-  (init [this {:keys [http-port http-handler]}]
+  (init [this {:keys [http-port http-handler http-request-logs?]}]
     (assert http-port)
     (assert http-handler)
 
-    (let [server (jetty/run-jetty http-handler
-                                  {:port http-port :configurator graceful-restart :join? false})
+    (let [server (run-jetty-hacked {:port http-port}
+                                   http-handler
+                                   (when http-request-logs? (request-log-handler)))
           hostname (.getHostName (java.net.InetAddress/getLocalHost))]
+
       (log/info (format "Ring started on %s:%s. Watch out for stones." hostname http-port))
       (assoc this :server server)))
 
