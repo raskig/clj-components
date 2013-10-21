a296b052e0bbaee760a998ddcc929d4aa596bbd2
Jon Pither <jon.pither@gmail.com>
2013-10-21 15:46:22 +0100

    proper snapshot ver

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-components "0.1.3-beta13-SNAPSHOT"
+(defproject clj-components "0.1.3-beta14-SNAPSHOT"
   :description "Component lifecycle management lib based off Avout/Zookeeper"
   :url "https://github.com/MailOnline/clj-components"
   :license {:name "Eclipse Public License"
