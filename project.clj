85df38a600f74de535123c0027bfd6f540591d40
Jon Pither <jon.pither@gmail.com>
2013-10-22 16:43:50 +0100

    tiny bit more config changes - have a space after DEBUG

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-components "0.1.3-beta17-SNAPSHOT"
+(defproject clj-components "0.1.3-beta18"
   :description "Component lifecycle management lib based off Avout/Zookeeper"
   :url "https://github.com/MailOnline/clj-components"
   :license {:name "Eclipse Public License"
