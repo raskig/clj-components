fdc318f694779030e2bfa38b607882b41b42f63e
Antonio Terreno <antonio.terreno@gmail.com>
2013-12-19 12:45:11 +0000

    archiving access logs inside logs folder to avoid messy folder structure

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-components "0.1.5-beta2"
+(defproject clj-components "0.1.5-beta3"
   :description "Component lifecycle management lib based off Avout/Zookeeper"
   :url "https://github.com/MailOnline/clj-components"
   :license {:name "Eclipse Public License"
