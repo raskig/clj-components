12d3cc221b07a9e31a6e8bce9535258bcbdb97c8
Jon Pither <jon.pither@gmail.com>
2014-01-07 16:27:21 +0000

    correct versioning for this branch

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-components "0.1.5-beta9"
+(defproject clj-components "0.2.0-alpha-1-SNAPSHOT"
   :description "Component lifecycle management lib based off Avout/Zookeeper"
   :url "https://github.com/MailOnline/clj-components"
   :license {:name "Eclipse Public License"
