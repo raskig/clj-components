a2cc73e1166638c32a96e10342110d0c88f91a25
Antonio Terreno <antonio.terreno@gmail.com>
2014-01-09 15:20:23 +0000

    bumping version & rolling back the riemann connection to tcp since udp seems to not work

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-components "0.1.5-beta9"
+(defproject clj-components "0.1.5-beta10"
   :description "Component lifecycle management lib based off Avout/Zookeeper"
   :url "https://github.com/MailOnline/clj-components"
   :license {:name "Eclipse Public License"
