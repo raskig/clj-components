037bdb7b7132791d8e4fa2a8adbc8ac8f78168bc
Jon Pither <jon.pither@gmail.com>
2014-01-09 13:09:51 +0000

    handy fn

--- dev/project_repl/components.clj
+++ dev/project_repl/components.clj
@@ -1,9 +1,13 @@
 (ns project-repl.components
   (:require [clj-components.components.elasticsearch]
-            [clj-components.bootstrap]))
+            [clj-components.bootstrap]
+            [clj-components.config]))
 
 (defn init []
   (clj-components.bootstrap/init! nil {} [clj-components.components.elasticsearch/->ElasticSearchComponent]))
 
 ;; (avout.core/dosync!! c (avout.core/alter!! (:settings @(:config s)) assoc :foo :bar))
 ;;  (def c (:client @(:config s)))
+
+(defn watcher-totals [s]
+  (clj-components.config/zk-watcher-totals (:client @(:config s))))
