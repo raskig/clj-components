8dc6afb96aaf42c5ecdb7367ff7a93034aee038d
Jon Pither <jon.pither@gmail.com>
2014-01-09 10:04:03 +0000

    friendlier

--- dev/project_repl/components.clj
+++ dev/project_repl/components.clj
@@ -1,9 +1,9 @@
 (ns project-repl.components
-  (:require [clj-components.full-registry]
+  (:require [clj-components.components.elasticsearch]
             [clj-components.bootstrap]))
 
 (defn init []
-  (clj-components.bootstrap/init! nil {} clj-components.full-registry/constructors))
+  (clj-components.bootstrap/init! nil {} [clj-components.components.elasticsearch/->ElasticSearchComponent]))
 
 ;; (avout.core/dosync!! c (avout.core/alter!! (:settings @(:config s)) assoc :foo :bar))
 ;;  (def c (:client @(:config s)))
