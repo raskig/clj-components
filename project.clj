96a2cee29a738c3e0dfb432d0ddc9bec34665826
Fraz <frazer.irving@mailonline.co.uk>
2014-03-06 21:31:39 +1100

    ups version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-components "0.2.0-alpha-12"
+(defproject clj-components "0.2.0-alpha-13"
   :description "Component lifecycle management lib based off Avout/Zookeeper"
   :url "https://github.com/MailOnline/clj-components"
   :license {:name "Eclipse Public License"
